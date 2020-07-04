441af312dffd0f758bb8d6ee56838cac9df562c7
Wesley <weslcld@gmail.com>
2020-07-04 13:56:58 -0300

    Ajustes no router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import ClasseInicial from './pages/ClasseInicial';
-import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import { HashRouter as Router , Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/HearthStone-Manager/deck" children={<Home />} />
-        <Route path="/HearthStone-Manager" children={<ClasseInicial />} />
+        <Route path="/deck" children={<Home />} />
+        <Route path="/" children={<ClasseInicial />} />
       </Switch>
     </Router>
   );

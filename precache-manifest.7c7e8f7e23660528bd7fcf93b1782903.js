fba4791d3ebf806d321ed950e982395f51517654
Wesley <weslcld@gmail.com>
2020-07-04 12:49:46 -0300

    Updates

---
+++ precache-manifest.7c7e8f7e23660528bd7fcf93b1782903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec717fb9f4ef2feea0777b6750c7c4be",
+    "url": "/HearthStone-Manager/index.html"
+  },
+  {
+    "revision": "5996718c9a1332ab715c",
+    "url": "/HearthStone-Manager/static/css/main.584d7ee5.chunk.css"
+  },
+  {
+    "revision": "fb2ad9a54379488fcda7",
+    "url": "/HearthStone-Manager/static/js/2.347edbe5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HearthStone-Manager/static/js/2.347edbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5996718c9a1332ab715c",
+    "url": "/HearthStone-Manager/static/js/main.5d343216.chunk.js"
+  },
+  {
+    "revision": "3cf99936d6e75fbe99af",
+    "url": "/HearthStone-Manager/static/js/runtime-main.ec22a421.js"
+  }
+]);
